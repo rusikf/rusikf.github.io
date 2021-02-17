1e183d9fc8549298787fe795a1496029a5ecf85a
Ruslan Korolev <great1672@gmail.com>
2021-02-17 17:25:22 +0200

    Fix homepage css for mobile - remove useless scroll (#4)

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import me from './images/me.jpeg';
 export default function About() {
   return (
     <div className="row description-block align-items-center">
-      <div className='text-start col-12 col-sm-6 d-flex flex-column'>
+      <div className='text-md-start col-12 col-sm-6 d-flex flex-column'>
         <p className='lead'>
           Hi! I am Ruslan Coroliov. 
           Full-stack web developer with experience in the backend: Ruby ( Ruby on Rails ) and frontend: Javascript ES6, ES5 (React, Vue)
