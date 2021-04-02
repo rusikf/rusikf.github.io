a92a46ad5fc41823008dd8f863f9b1db23b45878
Ruslan Korolev <great1672@gmail.com>
2021-04-02 22:12:21 +0300

    Add CSS certificate, move certifications from jsx to array of objects

--- src/Summary.js
+++ src/Summary.js
@@ -1,4 +1,52 @@
 export default function Summary() {
+  const certificates = [
+    {
+      title: 'CSS Certificate',
+      link: 'https://www.hackerrank.com/certificates/dfe091545ea4',
+      organization: 'HackerRank - April 2021'
+    },
+    {
+      title: 'Rest API (Intermediate) Certificate',
+      link: 'https://www.hackerrank.com/certificates/cd453396c5a2',
+      organization: 'HackerRank - March 2021'
+    },
+    {
+      title: 'JavaScript Algorithms and Data Structures',
+      link: 'https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/javascript-algorithms-and-data-structures',
+      organization: 'FreeCodeCamp - February 2021'
+    },
+    {
+      title: 'Responsive Web Design Certificate',
+      link: 'https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/responsive-web-design',
+      organization: 'FreeCodeCamp - October 2020'
+    },
+    {
+      title: 'JavaScript (Basic) Certificate',
+      link: 'https://www.hackerrank.com/certificates/98cbf4e9eb61',
+      organization: 'HackerRank - September 2020'
+    },
+    {
+      title: 'Problem Solving (Basic) Certificate',
+      link: 'https://www.hackerrank.com/certificates/0bae232a16cd',
+      organization: 'HackerRank - August 2020'
+    },
+    {
+      title: 'React ( Basic ) Certificate',
+      link: 'https://www.hackerrank.com/certificates/e2f4a0b104e2',
+      organization: 'HackerRank - July 2020'
+    },
+    {
+      title: 'Front End Libraries Certificate ( React | Jquery | Bootstrap | Sass | Redux )',
+      link: 'https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/front-end-libraries',
+      organization: 'FreeCodeCamp - 2020'
+    },
+    {
+      title: 'EF Standard English Test ( EF SET ) | Intermediate',
+      link: 'https://www.efset.org/cert/vbxnWV',
+      organization: 'EFSET - 2020'
+    }
+  ]
+
   return (
     <div className='row my-4'>
       <div className="col-md-4 offset-md-4 text-start">
@@ -6,8 +54,8 @@ export default function Summary() {
       <div>
         <h4>Full-stack web developer</h4>
         <p>Kodolabs - Remote - Since May 2016</p>
-          <p>Full-stack developer with Ruby on Rails experience 
-            with strong knowledge 
+          <p>Full-stack developer with Ruby on Rails experience
+            with strong knowledge
           in React, Vue and legacy jQuery and pure Javascript code</p><p>
           Participated in developing multiple startups from scratch to production.
           Features: Messaging API, Social media feed, complex filtering, Step builder on Vue.js,
@@ -19,8 +67,8 @@ export default function Summary() {
         <p>IMBS - Moscow, Russia - From June 2014 to May 2016
           Billing API integration + dashboard. FFmpeg video processing.
         </p>
-      </div> 
-      <div> 
+      </div>
+      <div>
         <h4>Junior Ruby developer</h4>
         <p>My course - Moscow - From January 2014 to June 2014</p>
         <p>Developed webinar platform from scratch ( backbone.js + rails + HTML
@@ -32,44 +80,14 @@ markups)</p>
       Moscow state university of mechanical engineering (MAMI) - Moscow
       </p>
       <h3>Certificates</h3>
-      <p>
-        <span style={{ display: 'block'}}>HackerRank - March 2021</span>
-        <a href="https://www.hackerrank.com/certificates/cd453396c5a2" target="_blank" rel="noopener noreferrer">Rest API (Intermediate) Certificate</a>
-      </p>
-       <p>
-        <span style={{ display: 'block'}}>FreeCodeCamp - February 2021</span>
-        <a href="https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/javascript-algorithms-and-data-structures" target="_blank" rel="noopener noreferrer">JavaScript Algorithms and Data Structures</a>
-      </p>
-      <p>
-        <span style={{ display: 'block'}}>FreeCodeCamp - October 2020</span>
-        <a href="https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/responsive-web-design" target="_blank" rel="noopener noreferrer">Responsive Web Design Certificate</a>
-      </p>
-      <p>
-        <span style={{ display: 'block'}}>HackerRank - September 2020</span>
-        <a href="https://www.hackerrank.com/certificates/98cbf4e9eb61" target="_blank" rel="noopener noreferrer">JavaScript (Basic) Certificate</a>
-      </p>
-
-      <p>
-        <span style={{ display: 'block'}}>HackerRank - August 2020</span>
-        <a href="https://www.hackerrank.com/certificates/0bae232a16cd" target="_blank" rel="noopener noreferrer">Problem Solving (Basic) Certificate</a>
-      </p>
-
-      <p>
-        <span style={{ display: 'block'}}>HackerRank - July 2020</span>
-        <a href="https://www.hackerrank.com/certificates/e2f4a0b104e2" target="_blank" rel="noopener noreferrer">React ( Basic ) Certificate</a>
-      </p>
-
-      <p>
-        <span style={{ display: 'block'}}> FreeCodeCamp - 2020</span>
-        <a href="https://www.freecodecamp.org/certification/fcc49ceb0e6-b33e-4d74-8314-e27aa65db621/front-end-libraries" target="_blank" rel="noopener noreferrer">Front End Libraries Certificate ( React | Jquery | Bootstrap | Sass |
-      Redux )</a>
-      </p>
-
-      <p>
-        <span style={{ display: 'block'}}> EFSET - 2020</span>
-        <a href="https://www.efset.org/cert/vbxnWV" target="_blank" rel="noopener noreferrer">EF Standard English Test ( EF SET ) | Intermediate</a>
-      </p>
-
+      {
+        certificates.map(( { title, link, organization }) => {
+          return <p>
+            <span style={{ display: 'block'}}>{ organization }</span>
+            <a href={ link } target="_blank" rel="noopener noreferrer">{ title }</a>
+          </p>
+        })
+      }
       </div>
     </div>
   )
