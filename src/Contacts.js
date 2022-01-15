40f16f64b06fe7cb096dcd8c29be849eaa7b6e97
Ruslan Korolev <great1672@gmail.com>
2022-01-15 20:10:21 +0200

    Fix spelling errors, update photo, add SQL certiciate

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,10 +2,10 @@ export default function Contacts() {
   return (
     <div className="my-4">
       <p>
-        <a href="mailto: rusikf123@gmail.com">Send Email to rusikf123@gmail.com</a>
+        <a href="mailto: 19r2mctrg@mozmail.com">Send Email to me!</a>
       </p>
 
-      <a href="https://www.linkedin.com/in/ruslan-korolev/" target="_blank" rel="noopener noreferrer">Send message on LinkedIn</a>
+      <a href="https://www.linkedin.com/in/ruslan-korolev/" target="_blank" rel="noopener noreferrer">Send a message on LinkedIn</a>
     </div>
   )
 }
