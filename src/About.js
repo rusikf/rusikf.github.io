222bd053f7b4225d1cabab891fc693568e59aa7c
Ruslan Korolev <great1672@gmail.com>
2022-07-02 19:28:06 +0300

    Update experience, certificate + upgrade all libraries

--- src/About.js
+++ src/About.js
@@ -6,11 +6,11 @@ export default function About() {
       <div className='text-md-start col-12 col-sm-6 d-flex flex-column'>
         <p className='lead'>
           Hi! I am Ruslan Coroliov.
-          Full-stack web developer with experience in the backend: Ruby ( Ruby on Rails ) and frontend: Javascript ES6, ES5 (React, Vue)
+          Full-stack web developer with experience in the backend: Ruby ( Ruby on Rails ) and frontend: Typescript/Javascript (React, Vue, Plain JS)
         </p>
 
         <small>
-        The ability to learn new things with a mix of good work-life balance is part of the daily routine, that's why I love remote work, which helps me be productive with tools like Toggle, Pivotal Tracker, etc.
+        The ability to learn new things with a mix of good work-life balance is part of the daily routine, that's why I love remote work, which helps me be productive!
         </small>
         </div>
       <div className='col-12 col-sm-6'>
